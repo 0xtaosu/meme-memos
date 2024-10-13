a4777d7587747b96e3f5e7d829f0d64143eecb0d
0xtaosu <taosue314@gmail.com>
2024-10-13 09:56:36 +0800

    Add debug logging for performance analysis

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    hmr: {
+      overlay: false // 禁用错误覆盖
+    }
+  }
 })
