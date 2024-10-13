90bde234addf184b2a2963c072dc36c6ca762d7f
0xtaosu <taosue314@gmail.com>
2024-10-13 21:37:46 +0800

    add CORS origin

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: process.env.PRODUCTION_CLIENT_URL || 'http://localhost:3000',
+    origin: ["https://api.memegrimoire.xyz", 'https://www.memegrimoire.xyz', 'http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }));
